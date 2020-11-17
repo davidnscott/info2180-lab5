ffad4c46716795f481bfe31270d468ecaecaf9d7
David Scott <davidnscott.ja@gmail.com>
2020-11-17 15:24:50 -0500

    Added lookup any country feature

---
+++ world.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+	let search = document.getElementById("lookup");
+	search.addEventListener('click', function(e){
+		e.preventDefault();
+		var s = document.getElementById("country").value;
+		var req = new XMLHttpRequest();
+		req.onreadystatechange = function() {
+			if (req.readyState == XMLHttpRequest.DONE) {
+				if (req.status == 200){	
+					var r = req.responseText;
+					var result = document.getElementById('result');
+					result.innerHTML = r;
+				}else{
+					alert("Error!");
+				}
+			}
+		}
+		console.log(s);
+		req.open('GET', 'http://localhost/info2180-lab5/world.php?country='+s, true);
+		req.send();
+	});
+}

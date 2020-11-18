22990ddb323871f02cc10163d0c19becc2981a68
David Scott <davidnscott.ja@gmail.com>
2020-11-17 19:41:25 -0500

    Added display city in country

--- world.js
+++ world.js
@@ -1,5 +1,6 @@
 window.onload=function(){
 	let search = document.getElementById("lookup");
+	let searchc = document.getElementById("lookupc");
 	search.addEventListener('click', function(e){
 		e.preventDefault();
 		var s = document.getElementById("country").value;
@@ -17,6 +18,25 @@ window.onload=function(){
 		}
 		console.log(s);
 		req.open('GET', 'http://localhost/info2180-lab5/world.php?country='+s, true);
+		req.send();
+	});
+	searchc.addEventListener('click',function(e){
+		e.preventDefault();
+		var s = document.getElementById("country").value;
+		var req = new XMLHttpRequest();
+		req.onreadystatechange = function() {
+			if (req.readyState == XMLHttpRequest.DONE) {
+				if (req.status == 200){	
+					var r = req.responseText;
+					var result = document.getElementById('result');
+					result.innerHTML = r;
+				}else{
+					alert("Error!");
+				}
+			}
+		}
+		console.log(s);
+		req.open('GET', 'http://localhost/info2180-lab5/world.php?country='+s+'&context=cities', true);
 		req.send();
 	});
 }
--- world.php
+++ world.php
@@ -7,11 +7,35 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country = filter_input(INPUT_GET,'country',FILTER_SANITIZE_STRING);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$context = filter_input(INPUT_GET,'context',FILTER_SANITIZE_STRING);
+
+if ($context == "cities") {
+	$stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'");
+}
+else 
+{
+	$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
 <table>
+<?php if ($context == 'cities'):?>
+	<tr>
+		<th>Name</th>
+		<th>District</th>
+		<th>Population</th>
+	</tr>
+	<?php foreach ($results as $row): ?>
+	<tr>
+		<th><?= $row['name']; ?></th>
+		<th><?= $row['district']; ?></th>
+		<th><?= $row['population']; ?></th>
+	</tr>
+<?php endforeach; ?>
+
+<?php else: ?>
 	<tr>
 		<th>Name</th>
 		<th>Contintent</th>
@@ -26,4 +50,5 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		<th><?= $row['head_of_state']; ?></th>
 	</tr>
 <?php endforeach; ?>
+<?php endif ?>
 </table>
